9e954fb522c7d9d6641325c6b51fad98cfdeb23b
Владимир Гаврилов <saxar.1kg@mail.ru>
2022-10-29 22:05:51 +0300

    Добавьте файлы проекта.

---
+++ App1/Line.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    struct Line
+    {
+        public double coefK;
+        public double CoefK
+        {
+            get
+            {
+                return coefK;
+            }
+            set
+            {
+                if (value != 0)
+                {
+                    coefK = value;
+                }
+                else
+                {
+                    Console.WriteLine("Коэффициент k не может быть равен 0");
+                }
+            }
+        }
+
+        public double coefB { get; set; }
+        public static double CoefB { get; internal set; }
+
+        public Line(double CoefK, double CoefB)
+        {
+            this.coefK = CoefK;
+
+            this.coefB = CoefB;
+        }
+        public void Root()
+        {
+            Console.WriteLine("Решение уравнения. Х = {0}", (-CoefB) / CoefK);
+        } 
+    }
+}
+
---
+++ App1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    
+    class Program
+    {
+        Line
+        public static void Main(string[] args)
+        {
+            Line line = new Line();
+            Console.WriteLine("Решить линейное уравнение 0=kx+b ");
+            Console.Write("Укажите коэффициент k: ");
+            double Line.coefK = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Укажите коэффициент b: ");
+            double Line.coefB = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(Line.Root);
+            Console.ReadKey();
+            
+        }
+    }
+}
